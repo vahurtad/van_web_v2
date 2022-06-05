54f3fe29f92df92235d78aee37b6f875a5973e0c
Vanessa Hurtado <vahurtad@ucsc.edu>
2022-06-04 17:13:00 -0700

    chore: comparing NextJS Image vs img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import React from 'react';
 
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
@@ -13,10 +15,21 @@ const Index = () => {
         <Meta title={AppConfig.title} description={AppConfig.description} />
       }
     >
-      <img
-        src={`${router.basePath}/assets/images/van-typographytest.gif`}
-        alt="Van is displayed as animated 3D typography style"
-      />
+      <div>
+        <Image
+          src={`${router.basePath}/assets/images/van-typographytest.gif`}
+          alt="Van is displayed as animated 3D typography style"
+          width="500"
+          height="500"
+          layout="fill"
+          objectFit="contain"
+        />
+
+        <img
+          src={`${router.basePath}/assets/images/van-typographytest.gif`}
+          alt="Van is displayed as animated 3D typography style"
+        />
+      </div>
     </Main>
   );
 };
