a17306793148e4b78916f003341c8368ee3a15f0
Vanessa <vahurtad@ucsc.edu>
2022-09-05 18:10:23 -0700

    fix(image): using unoptimized={true}

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-// import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 import { Meta } from '@/layouts/Meta';
@@ -7,10 +6,7 @@ import { Main } from '@/templates/Main';
 import { AppConfig } from '@/utils/AppConfig';
 
 const Index = () => {
-  // const router = useRouter();
   const [isLoading, setLoading] = useState(true);
-  // eslint-disable-next-line global-require
-  const logo = require('../assets/images/test.gif');
 
   return (
     <Main
@@ -26,36 +22,17 @@ const Index = () => {
               : 'grayscale-0 blur-none scale-100'
           }`}
           onLoadingComplete={() => setLoading(false)}
-          // eslint-disable-next-line global-require
-          src={require('../assets/images/van-typographytest.gif')}
+          src={`/van-typographytest.gif`}
           alt="Van is displayed as animated 3D typography style"
           layout="responsive"
           quality="100"
           priority
           width="1200"
           height="700"
+          // https://uploadcare.com/blog/next-js-image-optimization/
+          unoptimized={true}
+          loading="eager"
         />
-        <img
-          className={`duration-700 ease-in-out ${
-            isLoading
-              ? 'grayscale-0 blur-md scale-125'
-              : 'grayscale-0 blur-none scale-100'
-          }`}
-          onLoad={() => setLoading(false)}
-          src={logo.src}
-          alt="Van is displayed as animated 3D typography style"
-        />
-
-        <div
-          className={`imagebackground w-16 duration-700 ease-in-out md:w-32 lg:w-48 ${
-            isLoading
-              ? 'grayscale-0 blur-md scale-125'
-              : 'grayscale-0 blur-none scale-100'
-          }`}
-          onLoad={() => setLoading(false)}
-        >
-          HELLO
-        </div>
       </div>
     </Main>
   );
